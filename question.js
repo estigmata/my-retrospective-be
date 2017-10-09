0bf2cfd395ee4f2d4a2b72288d198a7a1080b816
estigmata <nestor.otondo@gmail.com>
2017-10-09 17:41:45 -0400

    add main schema

---
+++ question.js
@@ -0,0 +1,105 @@
+{
+	name: "Dev22Retro01",
+	max_rates: 5,
+	categories: [
+		{
+			name: "Went Well",
+			items: [
+				{
+					summary: "Lorem Ipsum",
+					rates: [
+						{
+							user: "Unknow B",
+							quantity: 1
+						},
+						{
+							user: "Unknow A",
+							quantity: 1
+						}
+					],
+				},			
+				{
+					summary: "Dolor sit amet",
+					rates: [
+						{
+							user: "Unknow A",
+							quantity: 1
+						}
+					],
+				},
+				{
+					summary: "Consetetur bella horrida bella",
+					rates: [
+						{
+							user: "Unknow B",
+							quantity: 1
+						}
+					],
+				},
+				{
+					summary: "Fotis fortuna audibat",
+					rates: [
+						{
+							user: "Unknow C",
+							quantity: 1
+						}
+					],
+				}
+			]
+		},
+		{
+			name: "To Improve",
+			items: [
+				{
+					summary: "Donec tempus mauris nisi",
+					rates: [
+						{
+							user: "Unknow A",
+							quantity: 1
+						},
+						{
+							user: "Unknow C",
+							quantity: 2
+						}
+					],
+				},			
+				{
+					summary: "Donec varius magna eu",
+					rates: [
+						{
+							user: "Unknow C",
+							quantity: 1
+						}
+					],
+				},
+				{
+					summary: "Nam congue sed dolor quis vulputate",
+					rates: [
+						{
+							user: "Unknow A",
+							quantity: 1
+						},
+						{
+							user: "Unknow B,
+							quantity: 1
+						}
+					],
+				}
+			]
+		}
+	]
+	action_items: [
+		{
+			name: "Fortis fortuna audibat",
+			owner: "Unknow A"
+		},
+		{
+			name: "Vini Vidi Vinchi",
+			owner: "Unknow A"
+		},
+		{
+			name: "Fusce at turpis sit amet lectus commodo gravida",
+			owner: "Unknow A"
+		}
+	]
+}
